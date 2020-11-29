2bf8ef5314aa6042da7c8f55cbb4349a9005c3cb
antobrines <antonin.brines@outlook.fr>
2020-11-29 18:23:23 +0100

    Questions 2 and 3 with exception

--- src/main/java/com/audiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/audiolibrary/web/controller/ArtistController.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +20,33 @@ public class ArtistController {
 
     @Autowired
     private ArtistRepository artistRepository;
+
+    private void ExceptionForPagination(String name, Integer page, Integer size, Sort.Direction sortDirection, String sortProperty){
+        List<String> sortPropertyList = Arrays.asList("name", "id");
+        int maxPage;
+        if(!sortPropertyList.contains(sortProperty)){
+            throw new IllegalArgumentException("Le tri fonctionne uniquement sur 'id' et 'name'");
+        }
+
+        if (size < 2 || size > 50)  {
+            throw new IllegalArgumentException("La taille des pages doit être comprise entre 1 et 50 !");
+        }
+
+        if (name != null){
+            maxPage = artistRepository.findAllByNameContaining(name, PageRequest.of(
+                    page,
+                    size,
+                    sortDirection,
+                    sortProperty)
+            ).getTotalPages() - 1;
+        }else{
+            maxPage = (int) artistRepository.count() / size - 1;
+        }
+
+        if (page < 0 || page > maxPage){
+            throw new IllegalArgumentException("Le numéro de page ne peut être inférieur à 0 ou supérieur à " + maxPage);
+        }
+    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Artist getArtistById(@PathVariable("id") Integer id){
@@ -36,6 +65,8 @@ public class ArtistController {
             @RequestParam(value = "sortProperty", defaultValue = "name") String sortProperty,
             @RequestParam(value = "sortDirection", defaultValue = "ASC") Sort.Direction sortDirection){
 
+        ExceptionForPagination(name, page, size, sortDirection, sortProperty);
+
         return artistRepository.findAllByNameContaining(name, PageRequest.of(page, size, sortDirection, sortProperty));
     }
 
@@ -45,6 +76,8 @@ public class ArtistController {
             @RequestParam(value = "size", defaultValue = "10") Integer size,
             @RequestParam(value = "sortProperty", defaultValue = "name") String sortProperty,
             @RequestParam(value = "sortDirection", defaultValue = "ASC") Sort.Direction sortDirection){
+
+        ExceptionForPagination(null, page, size, sortDirection, sortProperty);
 
         return artistRepository.findAll(PageRequest.of(page, size, sortDirection, sortProperty));
     }
