239727062b45c2cb2580ebd6b110f7921bb78ff3
antobrines <antonin.brines@outlook.fr>
2020-11-29 17:56:37 +0100

    Question 2 without exception

--- src/main/java/com/audiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/audiolibrary/web/controller/ArtistController.java
@@ -4,6 +4,9 @@ package com.audiolibrary.web.controller;
 import com.audiolibrary.web.model.Artist;
 import com.audiolibrary.web.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
@@ -23,6 +26,17 @@ public class ArtistController {
             return artist.get();
         }
         throw new EntityNotFoundException("L'artiste d'identifiant " + id + " n'existe pas.");
+    }
+
+    @RequestMapping(value = "", params = {"name"})
+    public Page<Artist> findArtistWithName(
+            @RequestParam("name") String name,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size,
+            @RequestParam(value = "sortProperty", defaultValue = "name") String sortProperty,
+            @RequestParam(value = "sortDirection", defaultValue = "ASC") Sort.Direction sortDirection){
+
+        return artistRepository.findAllByNameContaining(name, PageRequest.of(page, size, sortDirection, sortProperty));
     }
 
 }
--- src/main/java/com/audiolibrary/web/repository/ArtistRepository.java
+++ src/main/java/com/audiolibrary/web/repository/ArtistRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArtistRepository extends JpaRepository<Artist, Integer> {
+
+    Page<Artist> findAllByNameContaining(String name, Pageable pageable);
+
 }
